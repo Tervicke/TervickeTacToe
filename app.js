c39fba94a6d2da6ce2d08622472ab972a6e9908c
tervicke <ameywpatil@gmail.com>
2024-07-20 17:01:15 +0530

    comment out the testing code

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const maxReconnectAttempts = 10;
 const reconnectInterval = 3000;
 
 function connectWebSocket(){
-	//socket = new WebSocket("wss://tervicketactoe.onrender.com/ws");
-	socket = new WebSocket("ws://localhost:5000/ws")
+	socket = new WebSocket("wss://tervicketactoe.onrender.com/ws");
+	//socket = new WebSocket("ws://localhost:5000/ws")
 	
 	socket.onopen = function(){
 		console.log("Socket is now open");
